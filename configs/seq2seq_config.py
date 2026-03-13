51f22c101ac37f8da57f727df4e0433ee4426b53
Suriya Lakshmi M <suriya-nts0309@suriya-pt7984.cbe.zohocorpin.com>
2026-03-13 17:46:50 +0530

    Remove unused configs

--- configs/seq2seq_config.py
+++ configs/seq2seq_config.py
@@ -1,19 +1,15 @@
-VOCAB_SIZE = 10
-EMBED_SIZE = 7
-HIDDEN_SIZE = 5
-NUM_OF_SAMPLES = 300
-MAX_SRC_LEN = 20
+EMBED_SIZE = 300
+HIDDEN_SIZE = 256
 
 PAD = 0
 SOS = 1
 EOS = 9
 
 
-TRAIN_CONFIG = {"BATCH_SIZE": 2, "LR": 0.001, "EPOCHS": 10}
+TRAIN_CONFIG = {"BATCH_SIZE": 32, "LR": 0.001, "EPOCHS": 5}
 
 ENCODER_CONFIG = {
     "type": "lstm",
-    "vocab_size": VOCAB_SIZE,
     "embed_size": EMBED_SIZE,
     "hidden_size": HIDDEN_SIZE,
     "layers": 1,
@@ -22,7 +18,6 @@ ENCODER_CONFIG = {
 
 DECODER_CONFIG = {
     "type": "lstm",
-    "vocab_size": VOCAB_SIZE,
     "embed_size": EMBED_SIZE,
     "hidden_size": HIDDEN_SIZE,
     "layers": 1,
