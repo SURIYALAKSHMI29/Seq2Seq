a76ce16cca083cd4d8c44f773c6140f03de15346
Suriya Lakshmi M <suriya-nts0309@suriya-pt7984.cbe.zohocorpin.com>
2026-03-16 23:15:47 +0530

    Modify scripts to support bpe tokenization

--- src/seq2seq/data/dataloader.py
+++ src/seq2seq/data/dataloader.py
@@ -1,29 +1,39 @@
 import torch
-from torch.utils.data import DataLoader, TensorDataset, random_split
+from torch.utils.data import DataLoader, TensorDataset
 import pickle
 
 from .load_dataset import load_colloquial_tamil_dataset
-from .preprocessing import preprocess_dataset, build_vocab
-from configs.seq2seq_config import TRAIN_CONFIG
+from .preprocessing import preprocess_dataset, build_vocab, build_tokenizer
+from configs.seq2seq_config import TRAIN_CONFIG, PATHS, SRC_VOCAB_SIZE, TRG_VOCAB_SIZE
 
 
 def build_and_save_dataset():
     train, test = load_colloquial_tamil_dataset()
 
-    src_vocab = build_vocab(train["input"])
-    trg_vocab = build_vocab(train["output"])
+    src_tokenizer = build_tokenizer(
+        train["input"], vocab_size=SRC_VOCAB_SIZE, spl_tokens=["<PAD>", "<UNK>"]
+    )
+    trg_tokenizer = build_tokenizer(
+        train["output"],
+        vocab_size=TRG_VOCAB_SIZE,
+        spl_tokens=["<PAD>", "<UNk>", "<SOS>", "<EOS>"],
+    )
 
-    src, trg_in, trg_out = preprocess_dataset(train, src_vocab, trg_vocab)
-    src_test, trg_in_test, trg_out_test = preprocess_dataset(test, src_vocab, trg_vocab)
+    src, trg_in, trg_out = preprocess_dataset(train, src_tokenizer, trg_tokenizer)
+    src_test, trg_in_test, trg_out_test = preprocess_dataset(
+        test, src_tokenizer, trg_tokenizer
+    )
 
-    with open("data/processed/src_vocab_5k_data.pkl", "wb") as f:
-        pickle.dump(src_vocab, f)
+    with open("data/processed/src_tokenizer_5k_data.pkl", "wb") as f:
+        pickle.dump(src_tokenizer, f)
 
-    with open("data/processed/trg_vocab_5k_data.pkl", "wb") as f:
-        pickle.dump(trg_vocab, f)
+    with open("data/processed/trg_tokenizer_5k_data.pkl", "wb") as f:
+        pickle.dump(trg_tokenizer, f)
 
-    torch.save(((src, trg_in, trg_out)), "data/processed/train_5k_data.pt")
-    torch.save((src_test, trg_in_test, trg_out_test), "data/processed/test_5k_data.pt")
+    torch.save(((src, trg_in, trg_out)), "data/processed/train_5k_bpe_data.pt")
+    torch.save(
+        (src_test, trg_in_test, trg_out_test), "data/processed/val_5k_bpe_data.pt"
+    )
 
     print("Dataset built and saved")
 
@@ -34,18 +44,25 @@ def get_dataloader():
 
     print(batch_size)
 
-    train = torch.load("data/processed/train_5k_data.pt")
-    test = torch.load("data/processed/test_5k_data.pt")
+    train = torch.load(PATHS["TRAIN"])
+    val = torch.load(PATHS["VAL"])
     src_train, trg_in_train, trg_out_train = train
-    src_test, trg_in_test, trg_out_test = test
+    src_test, trg_in_val, trg_out_val = val
 
     train_dataset = TensorDataset(src_train, trg_in_train, trg_out_train)
-    test_dataset = TensorDataset(src_test, trg_in_test, trg_out_test)
+    val_dataset = TensorDataset(src_test, trg_in_val, trg_out_val)
 
-    src_vocab = pickle.load(open("data/processed/src_vocab_5k_data.pkl", "rb"))
-    trg_vocab = pickle.load(open("data/processed/trg_vocab_5k_data.pkl", "rb"))
+    src_vocab = pickle.load(open(PATHS["SRC_VOCAB"], "rb"))
+    trg_vocab = pickle.load(open(PATHS["TRG_VOCAB"], "rb"))
 
     train_loader = DataLoader(train_dataset, batch_size=batch_size, shuffle=True)
-    test_loader = DataLoader(test_dataset, batch_size=batch_size, shuffle=False)
+    val_loader = DataLoader(val_dataset, batch_size=batch_size, shuffle=False)
 
-    return (src_vocab, trg_vocab), train_loader, test_loader
+    return (src_vocab, trg_vocab), train_loader, val_loader
+    # return train_loader, val_loader
+
+
+def get_tokenizers():
+    src_tokenizer = pickle.load(open(PATHS["SRC_VOCAB"], "rb"))
+    trg_tokenizer = pickle.load(open(PATHS["TRG_VOCAB"], "rb"))
+    return src_tokenizer, trg_tokenizer
--- src/seq2seq/data/preprocessing.py
+++ src/seq2seq/data/preprocessing.py
@@ -1,5 +1,6 @@
 import torch
 from torch.nn.utils.rnn import pad_sequence
+from tokenizers import Tokenizer, models, trainers, pre_tokenizers
 
 
 def build_vocab(sentences):
@@ -13,6 +14,26 @@ def build_vocab(sentences):
         vocab[word] = len(vocab)
 
     return vocab
+
+
+## used for bpe
+def build_tokenizer(data, vocab_size, spl_tokens):
+    tokenizer = Tokenizer(models.BPE())
+    tokenizer.pre_tokenizer = pre_tokenizers.Whitespace()
+    trainer = trainers.BpeTrainer(vocab_size=vocab_size, special_tokens=spl_tokens)
+    tokenizer.train_from_iterator(data, trainer=trainer)
+    return tokenizer
+
+
+def encode_and_pad_bpe(sentences, tokenizer):
+    sequences = []
+    for sent in sentences:
+        seq = tokenizer.encode(sent).ids
+        sequences.append(torch.tensor(seq, dtype=torch.long))
+
+    return pad_sequence(
+        sequences, batch_first=True, padding_value=tokenizer.token_to_id("<PAD>")
+    )
 
 
 def encode_sentence(sentence, vocab):
@@ -33,15 +54,18 @@ def pad_data(sentences, vocab):
     return padded_seq
 
 
-def preprocess_dataset(dataset, src_vocab, trg_vocab):
+def preprocess_dataset(dataset, src_tokenizer, trg_tokenizer):
     src_sentences, trg_sentences = dataset["input"], dataset["output"]
     # print(src_sentences[5], trg_sentences[5])
 
     trg_sentences = [f"<SOS> {sentence} <EOS>" for sentence in trg_sentences]
     # print(trg_sentences[5])
 
-    src_padded = pad_data(src_sentences, src_vocab)
-    trg_padded = pad_data(trg_sentences, trg_vocab)
+    # src_padded = pad_data(src_sentences, src_vocab)
+    # trg_padded = pad_data(trg_sentences, trg_vocab)
+
+    src_padded = encode_and_pad_bpe(src_sentences, src_tokenizer)
+    trg_padded = encode_and_pad_bpe(trg_sentences, trg_tokenizer)
 
     src = src_padded
     trg_input = trg_padded[:, :-1]
