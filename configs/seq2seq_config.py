e944b30c746eb0138797e1cf2bbd6747d7bfd7bc
Suriya Lakshmi M <suriya-nts0309@suriya-pt7984.cbe.zohocorpin.com>
2026-03-18 17:41:31 +0530

    Include max_sentence_length, num of prefixes

--- configs/seq2seq_config.py
+++ configs/seq2seq_config.py
@@ -1,5 +1,5 @@
 EMBED_SIZE = 256
-HIDDEN_SIZE = 128
+HIDDEN_SIZE = 512
 
 SRC_VOCAB_SIZE = 2000
 TRG_VOCAB_SIZE = 2000
@@ -8,18 +8,26 @@ PAD = 0
 SOS = 1
 EOS = 9
 
+MAX_LEN = 10
+NUM_PREFIXES = 10
+## Avg target length = 8.72
+## Max target length = 55
+## Min target length = 3
 
-TRAIN_CONFIG = {"BATCH_SIZE": 32, "LR": 0.0001, "EPOCHS": 10}
+
+TRAIN_CONFIG = {"BATCH_SIZE": 64, "LR": 0.001, "EPOCHS": 5}
 PATHS = {
-    "TRAIN": "data/processed/train_10k_data.pt",
-    "VAL": "data/processed/val_10k_data.pt",
-    "SRC_VOCAB": "data/processed/src_vocab_10k_data.pkl",
-    "TRG_VOCAB": "data/processed/trg_vocab_10k_data.pkl",
-    "MODEL": "trained_models/seq2seq_v1.pth",
+    "TRAIN": "data/processed/ef_train_flt.pt",
+    "VAL": "data/processed/ef_val_flt.pt",
+    "SRC_VOCAB": "data/processed/ef_src_vocab_flt.pkl",
+    "TRG_VOCAB": "data/processed/ef_trg_vocab_flt.pkl",
+    "MODEL": "trained_models/seq2seq_ef_ep10_lr0.001_b64_dp12_emb256_h512_tf0.6_flt2_t37.pth",
+    "RAW_DATA": "data/raw/eng-fra.txt",
 }
 
 ENCODER_CONFIG = {
-    "type": "lstm",
+    "category": "encoder",
+    "model_name": "lstm",
     "vocab_size": SRC_VOCAB_SIZE,
     "embed_size": EMBED_SIZE,
     "hidden_size": HIDDEN_SIZE,
@@ -28,7 +36,8 @@ ENCODER_CONFIG = {
 }
 
 DECODER_CONFIG = {
-    "type": "lstm",
+    "category": "decoder",
+    "model_name": "lstm",
     "vocab_size": TRG_VOCAB_SIZE,
     "embed_size": EMBED_SIZE,
     "hidden_size": HIDDEN_SIZE,
