a014109aec55bc32ff8a92f31beb731ae2c88d71
Suriya Lakshmi M <suriya-nts0309@suriya-pt7984.cbe.zohocorpin.com>
2026-03-25 00:31:04 +0530

    generalize architecture for modular enc-dec compatibility

--- seq2seq/modules/seq2seq.py
+++ seq2seq/modules/seq2seq.py
@@ -16,35 +16,75 @@ class Seq2Seq(nn.Module):
         self.encoder_config = encoder_config
         self.decoder_config = decoder_config
 
-        if encoder_config.bidirectional:
-            self.enc_out_proj = nn.Linear(
-                encoder_config.hidden_size * 2, encoder_config.hidden_size
-            )
-            self.state_proj = nn.Linear(
-                encoder_config.hidden_size * 2, encoder_config.hidden_size
-            )
+        enc_dim = self.encoder.output_dim
+        dec_dim = self.decoder.input_dim
 
-        if encoder_config.hidden_size != decoder_config.hidden_size:
-            self.enc_out_hidden_proj = nn.Linear(
-                encoder_config.hidden_size, decoder_config.hidden_size
-            )
-            self.state_hidden_proj = nn.Linear(
-                encoder_config.hidden_size, decoder_config.hidden_size
-            )
+        self.enc_out_proj: nn.Module = None
+        self.state_proj: nn.Module = None
+
+        if enc_dim != dec_dim:
+            self.enc_out_proj = nn.Linear(enc_dim, dec_dim)
+            self.state_proj = nn.Linear(enc_dim, dec_dim)
+
+        # if encoder_config.bidirectional:
+        #     self.enc_out_proj = nn.Linear(
+        #         encoder_config.hidden_size * 2, encoder_config.hidden_size
+        #     )
+        #     self.state_proj = nn.Linear(
+        #         encoder_config.hidden_size * 2, encoder_config.hidden_size
+        #     )
+
+        # if encoder_config.hidden_size != decoder_config.hidden_size:
+        #     self.enc_out_hidden_proj = nn.Linear(
+        #         encoder_config.hidden_size, decoder_config.hidden_size
+        #     )
+        #     self.state_hidden_proj = nn.Linear(
+        #         encoder_config.hidden_size, decoder_config.hidden_size
+        #     )
 
     def transform_encoder_state(self, outputs, hidden, cell):
         ## transform encoder state if bidirectional
-        if self.encoder_config.bidirectional:
+        # if self.encoder_config.bidirectional:
 
-            # outputs are used in decoder attn
-            # decoder hidden size = hidden size, to match the dimensions in dot prod, output is transformed
+        #     # outputs are used in decoder attn
+        #     # decoder hidden size = hidden size, to match the dimensions in dot prod, output is transformed
+        #     outputs = self.enc_out_proj(outputs)
+
+        #     # project 2H → H, only if decoder is not bidirectional
+        #     if (
+        #         hasattr(self.decoder_config, "bidirectional")
+        #         and not self.decoder_config.bidirectional
+        #     ):
+        #         num_layers, batch, hidden_size = hidden.shape
+        #         num_layers = num_layers // 2
+
+        #         # reshape to separate directions (frwd & bwd)
+        #         hidden = hidden.view(num_layers, 2, batch, hidden_size)
+
+        #         # concatenate frwd + bwd
+        #         hidden = torch.cat((hidden[:, 0], hidden[:, 1]), dim=2)
+
+        #         # project 2H → H
+        #         hidden = self.state_proj(hidden)
+
+        #         if cell is not None:
+        #             cell = cell.view(num_layers, 2, batch, hidden_size)
+        #             cell = torch.cat((cell[:, 0], cell[:, 1]), dim=2)
+        #             cell = self.state_proj(cell)
+
+        # if hasattr(self, "enc_out_hidden_proj"):
+        #     outputs = self.enc_out_hidden_proj(outputs)
+
+        #     if hidden is not None:
+        #         hidden = self.state_hidden_proj(hidden)
+
+        #     if cell is not None:
+        #         cell = self.state_hidden_proj(cell)
+
+        if self.enc_out_proj is not None:
             outputs = self.enc_out_proj(outputs)
 
-            # project 2H → H, only if decoder is not bidirectional
-            if (
-                hasattr(self.decoder_config, "bidirectional")
-                and not self.decoder_config.bidirectional
-            ):
+            if hidden is not None and self.encoder_config.bidirectional:
                 num_layers, batch, hidden_size = hidden.shape
                 num_layers = num_layers // 2
 
@@ -62,26 +102,12 @@ class Seq2Seq(nn.Module):
                     cell = torch.cat((cell[:, 0], cell[:, 1]), dim=2)
                     cell = self.state_proj(cell)
 
-        if hasattr(self, "enc_out_hidden_proj"):
-            outputs = self.enc_out_hidden_proj(outputs)
-
-            if hidden is not None:
-                hidden = self.state_hidden_proj(hidden)
-
-            if cell is not None:
-                cell = self.state_hidden_proj(cell)
-
         return outputs, hidden, cell
 
     def adjust_layers(self, outputs, hidden, cell):
         ## if enc and dec have same num of layers -> no issue
         # if enc_layers > dec_layers -> return top dec_layers
         # if enc_layers < dec_layers -> repeat last layer
-
-        if not hasattr(self.decoder_config, "layers") or not hasattr(
-            self.encoder_config, "layers"
-        ):
-            return outputs, hidden, cell
 
         enc_layers = hidden.size(0)
         dec_layers = self.decoder_config.layers
@@ -119,13 +145,13 @@ class Seq2Seq(nn.Module):
             encoder_outputs, hidden, cell
         )
 
-        if hidden is not None:
+        if hidden is not None and self.decoder.needs_hidden:
             # matching encoder and decoder layers
             encoder_outputs, hidden, cell = self.adjust_layers(
                 encoder_outputs, hidden, cell
             )
 
-        hidden_cell = (hidden, cell) if hidden is not None else None
+        hidden_cell = (hidden, cell) if self.decoder.needs_hidden else None
 
         logits = self.decoder(
             trg, encoder_outputs, src_lengths, teacher_forcing, hidden_cell
