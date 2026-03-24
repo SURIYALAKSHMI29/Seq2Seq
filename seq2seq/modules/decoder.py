05a3d08043795048f0e69ba7273d78ffeec211e4
Suriya Lakshmi M <suriya-nts0309@suriya-pt7984.cbe.zohocorpin.com>
2026-03-25 00:30:02 +0530

    add AttentionDecoder, include input_dim property

--- seq2seq/modules/decoder.py
+++ seq2seq/modules/decoder.py
@@ -1,10 +1,13 @@
 import torch
 import torch.nn as nn
 from torch import Tensor
+import random
 
 from seq2seq.modules.attention import dot_attention
 from seq2seq.registry import REGISTRY
 from seq2seq.schemas import DecoderConfig
+from seq2seq.modules.attention import Attention
+from seq2seq.modules.encoding import positional_encoding
 
 
 class Decoder(nn.Module):
@@ -12,15 +15,11 @@ class Decoder(nn.Module):
         super().__init__()
         self.config = config
 
+        self.tf_ratio = config.teacher_forcing_ratio
+
         self.embedding = nn.Embedding(config.vocab_size, config.embed_size)
         self.dec_module: nn.Module | None = None
-        self.attention = dot_attention
-        linear_in = config.hidden_size
-        if config.bidirectional:
-            linear_in *= 2
-        if config.attention:
-            linear_in += config.hidden_size
-        self.fc = nn.Linear(linear_in, config.vocab_size)
+        self.fc: nn.Module | None = None
 
         # self.fc_proj = nn.Sequential(
         #     nn.Linear(linear_in, config.hidden_size),
@@ -30,6 +29,10 @@ class Decoder(nn.Module):
 
         self.embed_dropout = nn.Dropout(0.1)
         self.fc_dropout = nn.Dropout(0.2)
+
+    @property
+    def needs_hidden(self):
+        return False
 
 
 @REGISTRY.register("decoder", "lstm")
@@ -45,6 +48,22 @@ class LSTMDecoder(Decoder):
             bidirectional=config.bidirectional,
         )
 
+        self.attention = dot_attention
+        linear_in = config.hidden_size
+        if config.bidirectional:
+            linear_in *= 2
+        if config.attention:
+            linear_in += config.hidden_size
+        self.fc = nn.Linear(linear_in, config.vocab_size)
+
+    @property
+    def input_dim(self):
+        return self.config.hidden_size
+
+    @property
+    def needs_hidden(self):
+        return True
+
     def forward(
         self,
         trg_input: Tensor,
@@ -57,17 +76,15 @@ class LSTMDecoder(Decoder):
 
         outputs = []
 
-        if hidden_cell is None:
+        h_0, c_0 = hidden_cell
+        if h_0 is None:
             h_0 = (
                 encoder_hiddens.mean(dim=1)
                 .unsqueeze(0)
                 .repeat(self.config.layers, 1, 1)
             )
+        if c_0 is None:
             c_0 = torch.zeros_like(h_0)
-        else:
-            h_0, c_0 = hidden_cell
-            if c_0 is None:
-                c_0 = torch.zeros_like(h_0)
 
         hidden_cell = (h_0, c_0)
 
@@ -75,7 +92,9 @@ class LSTMDecoder(Decoder):
 
         for t in range(trg_len):
             # print("processing timestep", t)
-            if t == 0 or teacher_forcing:
+            use_teacher_forcing = teacher_forcing and (random.random() < self.tf_ratio)
+
+            if t == 0 or use_teacher_forcing:
                 input = trg_input[:, t].unsqueeze(1)
             else:
                 input = output.argmax(-1).unsqueeze(1)
@@ -120,4 +139,95 @@ class LSTMDecoder(Decoder):
         # print("Target length", trg_len)
 
         # print("Logits shape", logits.shape)
+        return logits
+
+
+@REGISTRY.register("decoder", "attention")
+class AttentionDecoder(Decoder):
+    def __init__(self, config: DecoderConfig):
+        super().__init__(config)
+        self.dec_module = Attention(config.embed_size)
+        self.cross_attn = Attention(config.embed_size)
+
+        self.pe = positional_encoding(config.max_trg_len, config.embed_size)
+
+        self.ffn = nn.Sequential(
+            nn.Linear(config.embed_size, 4 * config.embed_size),
+            nn.ReLU(),
+            nn.Linear(4 * config.embed_size, config.embed_size),
+        )
+
+        self.fc = nn.Linear(config.embed_size, config.vocab_size)
+
+        self.norm1 = nn.LayerNorm(config.embed_size)
+        self.norm2 = nn.LayerNorm(config.embed_size)
+        self.norm3 = nn.LayerNorm(config.embed_size)
+
+    @property
+    def input_dim(self):
+        return self.config.embed_size
+
+    def get_look_ahead_mask(self, trg_len):
+        mask = torch.ones(trg_len, trg_len)
+        mask = torch.tril(mask).bool()
+        return mask.unsqueeze(0)  # (1, trg_len, trg_len)
+
+    def forward(
+        self,
+        trg_input: Tensor,
+        encoder_hiddens: Tensor,
+        src_lengths: Tensor,
+        *args,
+        **kwargs
+    ):
+        # print("raw x", x.shape)
+        batch, trg_len = trg_input.shape
+        max_src_len = encoder_hiddens.shape[1]
+
+        # print(x.shape)  # batch, seq_len
+        x = self.embed_dropout(self.embedding(trg_input))  # batch, seq_len, embed
+        # print("x embeddings", x.shape)
+
+        ## positional encoding
+        x = x + self.pe[:, :trg_len, :]
+
+        look_ahead_mask = self.get_look_ahead_mask(trg_len)  # 1, trg_len, trg_len
+        trg_pad_mask = (trg_input != 0).unsqueeze(1)  # batch, 1, trg_len
+        trg_mask = trg_pad_mask & look_ahead_mask
+        # print("trg_mask", trg_mask.shape)   # batch, trg_len, trg_len
+
+        ## self - attention
+        self_attn_out = self.dec_module(
+            query=x,
+            key=x,
+            value=x,
+            mask=trg_mask,
+        )
+
+        ## residual connection
+        x = self.norm1(x + self_attn_out)
+
+        ## cross attention
+        src_mask = (
+            torch.arange(max_src_len).expand(batch, max_src_len)
+            < src_lengths.unsqueeze(1)
+        ).unsqueeze(1)
+
+        cross_attn_out = self.cross_attn(
+            query=x,
+            key=encoder_hiddens,
+            value=encoder_hiddens,
+            mask=src_mask,
+        )
+
+        x = self.norm2(x + cross_attn_out)
+
+        ## Feed forward network
+        ffn_out = self.ffn(x)
+        x = self.norm3(x + ffn_out)
+
+        # print("context:", context.shape)  # batch, seq_len, hidden_size
+        # print("context:", x[0, :5, :])
+
+        logits = self.fc_dropout(self.fc(x))
         return logits
